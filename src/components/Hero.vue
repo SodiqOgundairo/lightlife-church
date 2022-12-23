2ce016aa435d6428f62832c0599bc121e5e81a29
Opeyemi Ogundairo <kingmclamba1@gmail.com>
2022-12-23 17:24:52 +0100

    images updated

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,40 +1,49 @@
 <template>
   <div class="row">
-
     <div class="col overlay">
       <div class="row layer bg-vid pt-5">
         <!-- <video height="320" src="../assets/montage/llcMontage.mp4" type="video/mp4" playsinline autoplay muted loop>
         </video> -->
 
-        <img src="../assets/montage/llcMontage.gif" alt="llcMontage" class="img-fluid montage" />
+        <img
+          src="../assets/montage/llcMontage.gif"
+          alt="llcMontage"
+          class="img-fluid montage"
+        />
       </div>
     </div>
 
     <section>
       <div class="row bg-light py-5">
-        <div class="col-md-8 py-5 m-auto">
+        <div class="col-md-10 py-5 m-auto">
           <div class="card p-5 border-0 shadow-lg rounded-0">
             <div class="row g-0">
-              <!-- <div class="col-md-6 gap-3 d-flex align-self-center"> -->
-                <div class="card-body text-center">
-                  <p class="h1 card-title accent fw-bold">LIGHTLIFE CHURCH</p>
-                  <small class="text-muted h5 fst-italic">...Empowering Saints!</small>
-                  <p class="px-5 pt-5 card-text">
-                    LLC is not just a place, it is a people. It is a community where sinners, 
-                    the destitute, the despondent, the rejected, the depressed, the confused, 
-                    the hurting people can find salvation, love, hope joy, purpose, peace, acceptance and guidance. 
+              <div class="col-md-6 d-flex bg-accent">
+                <img src="../assets/img/welcome.jpg" class="img-fluid" alt="PJ" />
+              </div>
+
+              <div class="col-md-6 gap-3 d-flex align-self-center px-4">
+                <div>
+                  
+                  <div class="card-body ">
+                    <p class="h1 card-title theme fw-bold">LIGHTLIFE CHURCH</p>
+                    <small class="text-muted h5 fst-italic"
+                    >...Empowering Saints!</small
+                  >
+                  <p class="px-auto pt-5 text-start card-text">
+                    LLC is not just a place, it is a people. It is a community
+                    where sinners, the destitute, the despondent, the rejected,
+                    the depressed, the confused, the hurting people can find
+                    salvation, love, hope joy, purpose, peace, acceptance and
+                    guidance.
                   </p>
-                  <router-link to="/about" class=" btn-accent">Read More 
+                </div>
+                  
+                  <router-link to="/about" class="mt-3 btn-theme-gradient"
+                    >Read More
                   </router-link>
                 </div>
-              <!-- </div> -->
-              <!-- <div class="col-md-6 d-flex bg-accent">
-                <img
-                  src="../assets/img/llc.png"
-                  class="img-fluid"
-                  alt="PJ"
-                />
-              </div> -->
+              </div>
             </div>
           </div>
         </div>
@@ -64,24 +73,24 @@ export default {
   background: none !important;
 }
 
-.bg-vid{
+.bg-vid {
   height: 320;
 }
 
-@media (max-width:700px){
-  .bg-vid{
-    height: 240px
-  }
-
-  .montage{
-    height: 240px
-  }
-}
-
-.montage{
+.montage {
   object-fit: cover;
   object-position: center;
   height: 420px;
+}
+
+@media (max-width: 700px) {
+  .bg-vid {
+    height: 240px;
+  }
+
+  .montage {
+    height: 240px;
+  }
 }
 
 </style>
--- src/components/Section1.vue
+++ src/components/Section1.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div class=" overlay">
-      <div class=" layer py-5">
+      <div class=" py-5 cover bg-black bg-opacity-25">
       <div class="container">
       <div class="row my-5 m-auto">
           <div class="col-md-12 m-auto">
@@ -9,11 +9,11 @@
               <div
                 class="col-md-4 my-2 animate__animated animate__bounceInLeft"
               >
-                <div class="card bg-dark glass bg-opacity-25 shadow-lg p-2 border-0">
+                <div class="card bg-gradient glass bg-opacity-75 shadow-lg p-2 border-0">
                   <div class="card-body text-center">
-                    <p class="h3 accent fw-bold card-title">ASSIMILATION</p>
+                    <p class="h3 theme fw-bold card-title">ASSIMILATION</p>
                     <hr class="w-25 m-auto border-2 border border-dark" />
-                    <p class="card-text pt-2 text-white">
+                    <p class="card-text pt-2 text-white text-shadow">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Asperiores modi debitis facere doloribus ipsum illum
                       exercitationem repudiandae, sequi quam soluta nulla
@@ -28,12 +28,12 @@
                 class="col-md-4 my-2 animate__animated animate__bounceInLeft"
               >
                 <div
-                  class="card bg-dark glass bg-opacity-50 shadow-lg p-2 border-0"
+                  class="card bg-gradient glass bg-opacity-50 shadow-lg p-2 border-0"
                 >
                   <div class="card-body text-center">
                     <p class="h3 text-white fw-bold card-title">EMPOWERMENT</p>
                     <hr class="w-25 m-auto border-2 border border-light" />
-                    <p class="card-text text-white pt-2">
+                    <p class="card-text text-white text-shadow pt-2">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Asperiores modi debitis facere doloribus ipsum illum
                       exercitationem repudiandae, sequi quam soluta nulla
@@ -47,11 +47,11 @@
               <div
                 class="col-md-4 my-2 animate__animated animate__bounceInLeft"
               >
-                <div class="card bg-white glass bg-opacity-75 shadow-lg p-2 border-0">
+                <div class="card bg-gradient glass bg-opacity-50 shadow-lg p-2 border-0">
                   <div class="card-body text-center">
-                    <p class="h3 theme fw-bold card-title">RELEASE</p>
+                    <p class="h3 text-white fw-bold card-title">RELEASE</p>
                     <hr class="w-25 m-auto border-2 border border-dark" />
-                    <p class="card-text pt-2">
+                    <p class="card-text text-white text-shadow pt-2">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Asperiores modi debitis facere doloribus ipsum illum
                       exercitationem repudiandae, sequi quam soluta nulla
@@ -92,11 +92,12 @@ export default {
 }
 
 .overlay {
-  background-image: url("../assets/img/bg3.png");
+  background-image: url("../assets/img/cong-1.JPG");
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
 }
+
 
 .my-55a {
   margin-top: 500px;
