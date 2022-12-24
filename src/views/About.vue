c7f7498934b40190629dee176b59b184ba098d1a
Opeyemi Ogundairo <kingmclamba1@gmail.com>
2022-12-24 20:13:54 +0100

    more updates on the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -18,7 +18,7 @@
               <div class="col-md-6 gap-3 d-flex align-self-center">
                 <div class="card-body">
                   <p class="card-title h2 accent border-bottom head">
-                    WHO WE ARE?
+                    WHO WE ARE!
                   </p>
                   <p class="card-text">
                     LLC is not just a place, it is a people. It is a community
@@ -55,36 +55,350 @@
             </div>
           </div>
 
-          <div class="card border-1 border-theme p-md-5 my-3">
-            <div class="row g-0 align-self-center">
-              <div class="col-md-4 gap-3 m-3 ">
-                <div class="card-body bg-accent-gradient">
+          <div class="card border-1 border-theme m-auto p-md-5 my-3">
+            <div class="row g-0 align-self-center align-items-center">
+              <div class="col-md-4 gap-3 m-3 m-auto">
+                <div class="card-body bg-accent-gradient my-1 rounded-3">
                   <p class="card-title h3 text-white border-bottom">
                     OUR PURPOSE
                   </p>
-                  <p class="card-text text-white ">
+                  <p class="card-text text-white">
                     To Raise an Army of Disciples and Leaders
                   </p>
                 </div>
+
+                <div class="card-body bg-theme-gradient my-1 rounded-3">
+                  <p class="card-title h3 text-white border-bottom">
+                    OUR TARGET AUDIENCE
+                  </p>
+                  <p class="card-text text-white">The Unchurched</p>
+                </div>
               </div>
 
-              <div class="col-md-4 m-3">
-                <div class="card-body bg-theme-gradient">
+              <div class="col-md-7 m-3 m-auto">
+                <div class="card-body bg-theme rounded-3 p-4">
                   <p class="card-title h3 text-white border-bottom">
                     OUR PURPOSE STATEMENT
                   </p>
-                  <ul class="list-group">
-                    <li class="list-group-item ">To assimilate Citizens  into God’s Kingdom through Evangelism</li>
-                    <li>Empower/Groom into Maturity through Discipleship and Worship</li>
-                    <li>Release them into Ministry through Service</li>
+                  <ul class="list-group list-group-flush">
+                    <li
+                      class="list-group-item text-white p-1 bg-dark bg-opacity-25"
+                    >
+                      To assimilate Citizens into God’s Kingdom through
+                      Evangelism
+                    </li>
+                    <li
+                      class="list-group-item text-white p-1 bg-dark bg-opacity-25"
+                    >
+                      Empower/Groom into Maturity through Discipleship and
+                      Worship
+                    </li>
+                    <li
+                      class="list-group-item text-white p-1 bg-dark bg-opacity-25"
+                    >
+                      Release them into Ministry through Service
+                    </li>
                   </ul>
                 </div>
-                
+              </div>
+            </div>
+          </div>
+
+          <div class="card border-1 border-theme m-auto p-md-5 my-3">
+            <div class="row g-0 align-self-center m-auto align-items-center">
+              <div class="col-md-3 m-auto">
+                <div class="card-body bg-accent-gradient my-1 rounded-3">
+                  <p class="card-title h4 text-white border-bottom">
+                    OUR 3-FOLD MISSION STATEMENT
+                  </p>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item bg-none text-white">
+                      Assimilate
+                    </li>
+                    <li class="list-group-item bg-none text-white">
+                      Empowerment
+                    </li>
+                    <li class="list-group-item bg-none text-white">Release</li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-md-5 m-auto">
+                <div
+                  class="card-body border border-2 border-theme rounded-3 p-4"
+                >
+                  <p class="card-title h3 accent-2 border-bottom">VISION</p>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                      To Raise Kingdom Leaders in All Sphere of Human Influence.
+                    </li>
+                    <li class="list-group-item">
+                      To Help Saints Mature and Fulfil God’s Purpose.
+                    </li>
+                    <li class="list-group-item">
+                      To make disciple for Christ in Nations.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-md-3 m-auto">
+                <div class="card-body bg-theme-gradient my-1 rounded-3">
+                  <p class="card-title h3 text-white border-bottom">SUMMMARY</p>
+                  <p class="card-text text-white">
+                    To assimilate citizens (believers) into the God’s Kingdom
+                    (church), Empower them and Release them for Service
+                    (ministry)
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </section>
+
+    <!-- <section class="bg-white p-5">
+      <div class="bg-light col-md-11 m-auto shadow-lg p-5"> 
+      <div class="card my-3 p-md-5">
+        <table class="col-md-4 table table-hover">
+          <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">MISSION</th>
+            <th scope="col">STRATEGY</th>
+            <th scope="col">SCRIPTURE REF</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr class="table-secondary">
+            <td>1</td>
+            <th scope="row">Assimilate</th>
+            <td>Evangelism/Worship</td>
+            <td>Matthew 28:19</td>
+          </tr>
+          <tr class="table-light">
+            <td>2</td>
+            <th scope="row">Empowerment</th>
+            <td>Discipleship Training </td>
+            <td>       Eph 4:13-14, Mt 28:20</td>
+          </tr>
+          <tr class="table-secondary">
+            <td>3</td>
+            <th scope="row">Release</th>
+            <td>Ministry/Service </td>
+            <td> 2Tim 2:2,  Eph  4:12</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    </section> -->
+
+    <section class="">
+      <div class="row bg-white py-5">
+        <div class="col-md-10 bg-theme-gradient shadow-lg m-auto p-md-5">
+          <div class="card bg-white p-md-5 my-3">
+            <div class="row g-0 align-items-center">
+              <div class="col-md-6 d-flex">
+                <img
+                  src="../assets/img/IBK.jpg"
+                  class="img-fluid rounded-3 img-fit"
+                  alt="About LLC
+            "
+                />
+              </div>
+              <div class="col-md-6 gap-3 d-flex align-self-center">
+                <div class="card-body">
+                  <p class="card-title h2 accent-2 border-bottom head">
+                    OUR PHILOSOPHY
+                  </p>
+                  <p class="card-text">
+                    Lightlife church is a New Testament church. We believe in
+                    the five-fold ministry; Apostles, Prophets, Evangelists,
+                    Pastors and Teachers. We believe that for a church to be
+                    equipped, matured and come to the fullness of Christ, it
+                    must embrace and manifest all the five-fold ministry gifts.
+                    Ephesians 4:7-16
+                    <span class="d-block mt-2">
+                      We also believe that every believer is called to ministry
+                      to serve through their ministerial gifts. Also, we believe
+                      that every believer is born to be a leader. However, we
+                      lead by serving others. We take pleasure in serving and
+                      not being served. Matthew 20:24-28.
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="">
+      <div class="row bg-white py-5">
+        <div class="col-md-10 bg-accent bg-opacity-50 shadow-lg m-auto p-md-5">
+          <div class="card bg-white p-md-5 my-3">
+
+                <div class="card-body">
+                  <p class="card-title h2 accent border-bottom">
+                    OUR COMMITMENT
+                  </p>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                      Love
+                    </li>
+                    <li class="list-group-item">
+                      Excellence
+                    </li>
+                    <li class="list-group-item">
+                      Worship
+                    </li>
+                    <li class="list-group-item">
+                      Prayer
+                    </li>
+                    <li class="list-group-item">
+                      Building Relationships
+                    </li>
+                    <li class="list-group-item">
+                      Soul Winning
+                    </li>
+                    <li class="list-group-item">
+                      Humility
+                    </li>
+                    <li class="list-group-item">
+                      Hospitality
+                    </li>
+                    <li class="list-group-item">
+                      Training
+                    </li>
+                    <li class="list-group-item">
+                      Character building
+                    </li>
+                  </ul>
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="">
+      <div class="row bg-white py-5">
+        <div class="col-md-10 bg-light bg-opacity-50 shadow-lg m-auto p-md-5">
+          <div class="card bg-white border-1 border-theme p-md-5 my-3">
+
+                <div class="card-body">
+                  <p class="card-title h2 accent border-bottom">
+                    OUR AIMS
+                  </p>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                      To be conformed to the image of Christ. To know Him and be like Him in all things.
+                    </li>
+                    <li class="list-group-item">
+                      To equip every believers in LLC to fulfil their ministry and vision.
+                    </li>
+                    <li class="list-group-item">
+                      To ensure every believer function in their ministerial calling whether as apostles, prophets, evangelists, pastors or teachers for the equipping of other saints.
+                    </li>
+                    <li class="list-group-item">
+                      To reach the lost souls through evangelism and church planting the gospel of Christ.
+                    </li>
+                    <li class="list-group-item">
+                      To help saints live as a worthy ambassadors of Christ, living a life of love and worthy of emulation. 
+                    </li>
+                    <li class="list-group-item">
+                      To build churches in villages, towns and cities to reach the nations.
+                    </li>
+                    <li class="list-group-item">
+                      To cater for the poor and less privileges through our Lightlight Social Responsibility (LLCR) arm.
+                    </li>
+                  </ul>
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="">
+      <div class="row bg-white py-5">
+          <div class="card bg-theme p-md-5 my-3">
+
+                <div class="card-body">
+                  <p class="card-title h2 text-white border-bottom">
+                    EXPLANATION
+                  </p>
+                  <div class="col-md-8 m-auto bg-light rounded-3 p-3 p-lg-5">
+                    <div class="my-3">
+                      <p class="card-header theme"><span class="fw-bold"> 1.0 </span> ASSIMILATING CITIZENS</p>
+                      <p class='card-text'>We believe that the core purpose of Christ’s coming is to bring men (male & females) into God’s kingdom ( Matthew 4:17,28:19, Mark 16:14, Luke 9:60,  Hebrews 2:10). As a New Testament church, Jesus as commanded us to GO out and make Disciples of all nations through the preaching of the Gospel of Grace. Also, to shine as light in the midst of darkness (John 1:6-9, Matthew 5:13-14). 
+                      <span class="d-block">
+
+                        At LLC we believe it our core responsibility to win soul into God’s Kingdom. We believe in preaching the message of grace and love of God to sinners. Ephesian 1:13, 2:8-9, 3:2, Romans 1:16, 16:25, John 1:17, Luke 4:18, 2 Timothy 2:2, 1Pet 1:10, Colossians 1:23.
+                      </span>
+                    </p>
+                  </div>
+
+                  <div class="my-3">
+                    <p class="card-header theme"><span class="fw-bold"> 1.1 </span> STRATEGIES</p>
+                    <p class='card-text'> <span class="d-block fw-bolder">Evangelism</span>
+                      <span class="d-block">
+                        We are committed to evangelism either personal or corporate. Luke 4:18, Act 16:10, Romans 1:15, 10:15, 15:20, 10:14, 1Corinthians 1:23, 2Timothy 4:2. We believe that the church truly grows when lost souls come to Christ. 
+                      </span>
+                    </p>
+                  </div>
+
+                  <div class="my-3">
+                    <p class="card-header theme"><span class="fw-bold"> 1.2 </span> PLATFORMS</p>
+                    <ol class="list-group">
+                      <li class=list-group-item>One on One Evangelism</li>
+                      <li class=list-group-item>Outreaches in Schools</li>
+                      <li class=list-group-item>Crusades</li>
+                      <li class=list-group-item>Social Media</li>
+                      <li class=list-group-item>Etc </li>
+                    </ol>
+                  </div>
+                  
+                  <div class="my-3">
+                    <p class="card-header theme"><span class="fw-bold"> 1.3 </span> FOLLOW UP</p>
+                    <p class='card-text'> 
+                      <span class="d-block">
+                        We make sure we that the souls won to Christ are followed up until they are retained and established in faith. We also ensure they enrol in Citizens School (CS). These can be done through several means which includes
+                      </span>
+                    </p>
+                    <ol class="list-group">
+                      <li class=list-group-item>Visitation</li>
+                      <li class=list-group-item>Text messages</li>
+                      <li class=list-group-item>Social Media</li>
+                      <li class=list-group-item>Meeting their needs </li>
+                      <li class=list-group-item>Celebrating them always </li>
+                    </ol>
+                  </div>
+                  <p class="fst-italic italic alert alert-danger"> <span class="fw-bold">NOTE:</span> Our first timers and converts must be reached in less than 24 hours by the follow up team and the pastor. </p>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-white pb-lg-5 ">
+      <div class="col-md-10 m-3 p-md-5 bg-light m-auto">
+        <div class="card p-md-5 my-3 shadow-lg">
+          <div class="card-header"> <p class='card-title fw-bold theme h2'>CITIZENS COMMITMENT</p> </div>
+          <div class='card-body'>
+            <p class='card-title'> In LLC, the Citizens are regarded as our official members. A citizen then is committed to the following. </p>
+            <ol class="list-group">
+              <li class=list-group-item> <span class="fw-bold">Fellowship:  </span> Attends LLC programmes and events regularly</li>
+              <li class=list-group-item> <span class="fw-bold">Worship:  </span>  Must be committed to both personal and corporate worship of LLC</li>
+              <li class=list-group-item> <span class="fw-bold">Evangelism:  </span> This is core. A citizen must be committed to soul winning</li>
+              <li class=list-group-item> <span class="fw-bold">Prayer:  </span> Praying for the church and her leadership always</li>
+              <li class=list-group-item> <span class="fw-bold">Service:  </span> We believe there are no spectator in the church. As priest we must be committed to serving God and other saint in love and humility</li>
+              <li class=list-group-item> <span class="fw-bold">Financial Partnership:  </span> LLC citizens are financial partners with God in LLC. They are committed to giving money and material things for the furtherance of the gospel of the Gospel.</li>
+              <li class=list-group-item> <span class="fw-bold">Training:  </span> Citizens must have gone through Citizens School (CS}</li>
+            </ol>
+        </div>
+        </div>
+        </div>
     </section>
 
     <section>
@@ -161,5 +475,9 @@ export default {
 .rotate {
   -webkit-transform: scaleX(-1);
   transform: scaleX(-1);
+}
+
+.table {
+  overflow-x: scroll;
 }
 </style>
